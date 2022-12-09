4a27fd7d98cc02860faf505927db002bbd25e894
NaiHeKK <2197960230@qq.com>
2022-12-09 13:06:54 +0000

    初始化

---
+++ download/221c4c76-77bc-11ed-ab5f-0242ac110002.js
@@ -0,0 +1,25 @@
+/**
+* @author YuanKK
+ * @version v1.0.0
+* @create_at 2022-09-07 23:05:40
+* @description 随机抖音视频
+* @title 扭一扭
+* @platform qq wx tg pgm web
+* @rule ^nyn$
+* @rule ^扭一扭$
+ * @public false
+*/
+
+//sender
+const s = sender
+
+var url = "https://v.nrzj.vip/video.php"
+var {headers} = request({
+        url: url,
+        method:"get",
+        allowredirects: false,
+        headers:{
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+        }
+    })
+s.reply(video(headers.Location[0]))
