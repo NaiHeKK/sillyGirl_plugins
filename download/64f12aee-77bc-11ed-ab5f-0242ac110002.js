88103c8116ef4376498a4153d3b873e029e1fc94
NaiHeKK <2197960230@qq.com>
2022-12-09 15:12:20 +0000

    二次元

---
+++ download/64f12aee-77bc-11ed-ab5f-0242ac110002.js
@@ -0,0 +1,39 @@
+/**
+* @author YuanKK
+ * @version v1.0.0
+* @create_at 2022-09-07 23:14:19
+* @description 🐒这个人很懒什么都没有留下。
+* @title 二次元
+* @rule ^ACG$
+* @rule ^二次元$
+* @rule ^acg$
+ * @public false
+*/
+
+//sender
+const s = sender
+
+function main() {
+    let { headers, status } = request({
+        url: "https://api.ghser.com/random/api.php",
+        method: "get",
+        allowredirects: false,
+        headers: {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+        }
+    });
+    if ([302, 304].indexOf(status) != -1) {
+        let data = request({
+            url: "https://api.ghser.com/random/" + headers.Location,
+            method: "get",
+            allowredirects: false,
+            headers: {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+            }
+        });
+        var id = s.reply(image(data.headers.Location))
+        sleep(10000)
+        s.recallMessage(s.getMessageId(), id)
+    }
+}
+main()
