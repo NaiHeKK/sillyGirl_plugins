0f4a356e11a128af2c65ae794439aa1d62322e13
NaiHeKK <2197960230@qq.com>
2022-12-12 12:30:04 +0800

    syncGitTask

--- download/221c4c76-77bc-11ed-ab5f-0242ac110002.js
+++ download/221c4c76-77bc-11ed-ab5f-0242ac110002.js
@@ -21,5 +21,5 @@ var {headers} = request({
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
         }
     })
-console.log(headers.Location[0])
-s.reply(video(headers.Location[0]))
+console.log("nyn")
+s.reply(`${video(headers.Location[0])}`)
