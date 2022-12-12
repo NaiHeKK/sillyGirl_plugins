135d9630aeb19496f2d0651ca801d9974b0e44dd
NaiHeKK <2197960230@qq.com>
2022-12-12 10:30:03 +0800

    syncGitTask

--- download/221c4c76-77bc-11ed-ab5f-0242ac110002.js
+++ download/221c4c76-77bc-11ed-ab5f-0242ac110002.js
@@ -4,7 +4,6 @@
 * @create_at 2022-09-07 23:05:40
 * @description 随机抖音视频
 * @title 扭一扭
-* @platform qq wx tg pgm web
 * @rule ^nyn$
 * @rule ^扭一扭$
  * @public false
@@ -22,4 +21,5 @@ var {headers} = request({
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
         }
     })
+console.log(headers.Location[0])
 s.reply(video(headers.Location[0]))
